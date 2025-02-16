b7f19c8d01b9f81591b2895ff3a89060de24de62
Spucen1 <108100262+Spucen1@users.noreply.github.com>
2025-02-16 23:22:18 +0100

    what

--- personal/jsTest/jsTestV1.js
+++
@@ -1,8 +0,0 @@
-console.log("Výpočet obsahu obdĺžnika");
-let button = document.getElementById("button");
-button.addEventListener("click", function () {
-  let a = parseFloat(document.getElementById("a").value);
-  let b = parseFloat(document.getElementById("b").value);
-  let obsah = a * b;
-  button.innerText = obsah;
-});
