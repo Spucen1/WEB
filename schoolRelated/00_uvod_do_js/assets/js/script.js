8d921dfc93fa48f77ba43580b7dd05dc4fb3eb10
Spucen1 <108100262+Spucen1@users.noreply.github.com>
2025-11-18 09:25:57 +0100

    feat: implement button click functionality to update text content

--- schoolRelated/00_uvod_do_js/assets/js/script.js
+++ schoolRelated/00_uvod_do_js/assets/js/script.js
@@ -1,1 +1,12 @@
 console.log("Script načítaný správne!");
+
+const btn = document.getElementById("btn-bg");
+
+const text = document.getElementById("text");
+
+if (btn && text) {
+    btn.addEventListener("click", () => {
+        text.textContent = "commit and push";
+    });
+}
+
