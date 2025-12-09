357d957a81186e05da5b80c279acf4039b3ab0f0
Spucen1 <108100262+Spucen1@users.noreply.github.com>
2025-12-09 09:30:21 +0100

    fix: update number variable to -30 for correct parity and sign evaluation

--- schoolRelated/jstst/assets/script.js
+++ schoolRelated/jstst/assets/script.js
@@ -1,10 +1,18 @@
 console.log("Hello, World!");
 
-let number = 30;
+let number = -30;
 let idk = number % 2;
 
 if (idk === 0) {
     console.log("Even");
 } else {
     console.log("Odd");
+}
+
+if (number > 0) {
+    console.log("Positive");
+} else if (number < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
 }
